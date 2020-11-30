d80147007a7f81bfa14db6383845ded85979dbbe
Haruto Watanabe <w.haru10n@gmail.com>
2020-11-30 14:17:49 +0900

    Update 4_4.cpp

---
+++ chap04/4_4.cpp
@@ -0,0 +1,16 @@
+// ユーグリッドの互除法
+#include <iostream>
+using namespace std;
+
+int GCD(int m, int n) {
+  // ベースケース
+  if (n == 0) return m;
+  
+  // 再起呼び出し
+  return GCD(n, m % n);
+}
+
+int main() {
+  cout << GCD(51, 15) << endl; // 3 が出力される
+  cout << GCD(15, 51) << endl; // 3 が出力される
+}
